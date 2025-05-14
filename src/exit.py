8c1c3f192c709ba679f71c9d08bd51fa626397b1
Marcin Zub <marcin_zub@outlook.com>
2025-05-14 08:49:10 +0200

    feature: basic configuration file
    added .json configuration file with example parameters: 30 agents and 4
    exits

--- src/exit.py
+++ src/exit.py
@@ -4,7 +4,9 @@ from settings import *
 
 
 class Exit(pg.sprite.Sprite):
-    def __init__(self, game, x, y, width=EXIT_WIDTH, height=EXIT_HEIGHT, color=EXIT_COLOR):
+    def __init__(
+        self, game, x, y, width=EXIT_WIDTH, height=EXIT_HEIGHT, color=EXIT_COLOR
+    ):
         super().__init__()
         self.game = game
         self.image = pg.Surface((width, height))
